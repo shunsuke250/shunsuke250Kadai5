f6046d150a17cf0a138dcd6c1db25eeb8caec76a
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-06 22:53:52 +0900

    :sparkles: 割り算を計算し結果を出力する機能を追加

--- Calculator23-09-06/Calculator23-09-06/ViewController.swift
+++ Calculator23-09-06/Calculator23-09-06/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var dividendNumTextField: UITextField!
+    @IBOutlet private weak var divisorNumTextField: UITextField!
+    @IBOutlet private weak var divisionLabel: UILabel!
+    @IBOutlet private weak var calculateButton: UIButton!
+    @IBOutlet private weak var calculateResultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        divisionLabel.text = "÷"
+        calculateButton.setTitle("計算", for: .normal)
+        calculateResultLabel.text = ""
     }
 
+    @IBAction private func didTapCalculateButton(_ sender: UIButton) {
+        guard let dividendNum = Double(dividendNumTextField.text ?? ""),
+              let divisorNum = Double(divisorNumTextField.text ?? "")
+        else { return }
 
+        let result = dividendNum / divisorNum
+        calculateResultLabel.text = "\(result)"
+    }
 }
-
