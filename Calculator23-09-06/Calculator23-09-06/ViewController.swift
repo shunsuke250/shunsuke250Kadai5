428a5751de0cc936f3ed697250aece464dbe1cb9
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-08 23:41:23 +0900

    :sparkles: バリデーションでアラートを表示する機能を追加

--- Calculator23-09-06/Calculator23-09-06/ViewController.swift
+++ Calculator23-09-06/Calculator23-09-06/ViewController.swift
@@ -20,15 +20,36 @@ class ViewController: UIViewController {
     }
 
     @IBAction private func didTapCalculateButton(_ sender: UIButton) {
-        divisionResult()
-    }
-
-    private func divisionResult() {
-        guard let dividendNum = Double(dividendNumTextField.text ?? ""),
-              let divisorNum = Double(divisorNumTextField.text ?? "")
+        guard let dividendNum = dividendNumTextField.text,
+              let divisorNum = divisorNumTextField.text
         else { return }
 
-        let result = dividendNum / divisorNum
+        if dividendNum == "" {
+            alert(message: "割られる数を入力してください")
+
+            return
+        } else if divisorNum == "" {
+            alert(message: "割る数を入力してください")
+
+            return
+        } else if divisorNum == "0" {
+            alert(message: "割る数には0を入力しないでください")
+
+            return
+        }
+
+        let result = (Double(dividendNum) ?? 0) / (Double(divisorNum) ?? 0 )
         calculateResultLabel.text = "\(result)"
+
+    }
+
+    private func alert(message: String) {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default) { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }
+
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
     }
 }
